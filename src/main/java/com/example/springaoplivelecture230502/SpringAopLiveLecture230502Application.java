f04d8eda6441a6ee582346cdf37142faeb507f9e
Robbie <lecture@>
2023-05-01 17:56:36 +0900

    init commit

---
+++ src/main/java/com/example/springaoplivelecture230502/SpringAopLiveLecture230502Application.java
@@ -0,0 +1,13 @@
+package com.example.springaoplivelecture230502;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopLiveLecture230502Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAopLiveLecture230502Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springaoplivelecture230502/SpringAopLiveLecture230502ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springaoplivelecture230502;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopLiveLecture230502ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
